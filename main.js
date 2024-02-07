fc0f4ece775d1d312247a986461802e84e94aa34
Lgc <142270066+Lgcool45@users.noreply.github.com>
2024-02-07 13:14:06 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,73 +1,43 @@
-let switchCtn = document.querySelector("#switch-cnt");
-        let switchC1 = document.querySelector("#switch-c1");
-        let switchC2 = document.querySelector("#switch-c2");
-        let switchCircle = document.querySelectorAll(".switch__circle");
-        let switchBtn = document.querySelectorAll(".switch-btn");
-        let aContainer = document.querySelector("#a-container");
-        let bContainer = document.querySelector("#b-container");
-        let allButtons = document.querySelectorAll(".submit");
-        
-        let getButtons = (e) => e.preventDefault();
-        
-        let changeForm = (e) => {
-        
-            switchCtn.classList.add("is-gx");
-            setTimeout(function(){
-                switchCtn.classList.remove("is-gx");
-            }, 1500);
-        
-            switchCtn.classList.toggle("is-txr");
-            switchCircle[0].classList.toggle("is-txr");
-            switchCircle[1].classList.toggle("is-txr");
-        
-            switchC1.classList.toggle("is-hidden");
-            switchC2.classList.toggle("is-hidden");
-            aContainer.classList.toggle("is-txl");
-            bContainer.classList.toggle("is-txl");
-            bContainer.classList.toggle("is-z200");
-        };
-        
-        let mainF = (e) => {
-            for (var i = 0; i < allButtons.length; i++)
-                allButtons[i].addEventListener("click", getButtons );
-            for (var i = 0; i < switchBtn.length; i++)
-                switchBtn[i].addEventListener("click", changeForm);
-        };
-        
-        window.addEventListener("load", mainF);
-        
-        function redirectAndSave() {
-            const username = document.getElementById("username").value;
-            const password = document.getElementById("password").value;
-            localStorage.setItem("username", username);
-            localStorage.setItem("password", password);
-            window.location.href = "dashboard.html";
-        }
+$('.form').find('input, textarea').on('keyup blur focus', function (e) {
+  
+  var $this = $(this),
+      label = $this.prev('label');
 
-        function signIn() {
-            const enteredUsername = document.getElementById("username").value;
-            const enteredPassword = document.getElementById("password").value;
-
-            fetch('/login', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify({
-                    username: enteredUsername,
-                    password: enteredPassword
-                })
-            })
-            .then(response => {
-                if (response.ok) {
-                    window.location.href = "dashboard.html";
-                } else {
-                    alert("Invalid username/password combination");
-                }
-            })
-            .catch(error => {
-                console.error('Error:', error);
-                alert("An error occurred. Please try again.");
-            });
+	  if (e.type === 'keyup') {
+			if ($this.val() === '') {
+          label.removeClass('active highlight');
+        } else {
+          label.addClass('active highlight');
         }
-        alert("test")
+    } else if (e.type === 'blur') {
+    	if( $this.val() === '' ) {
+    		label.removeClass('active highlight'); 
+			} else {
+		    label.removeClass('highlight');   
+			}   
+    } else if (e.type === 'focus') {
+      
+      if( $this.val() === '' ) {
+    		label.removeClass('highlight'); 
+			} 
+      else if( $this.val() !== '' ) {
+		    label.addClass('highlight');
+			}
+    }
+
+});
+
+$('.tab a').on('click', function (e) {
+  
+  e.preventDefault();
+  
+  $(this).parent().addClass('active');
+  $(this).parent().siblings().removeClass('active');
+  
+  target = $(this).attr('href');
+
+  $('.tab-content > div').not(target).hide();
+  
+  $(target).fadeIn(600);
+  
+});
