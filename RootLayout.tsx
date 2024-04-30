d64caddf1be47eb24c78277e910e8274e3b0116d
Lgc <142270066+Lgcool45@users.noreply.github.com>
2024-04-30 12:46:13 -0400

    Update RootLayout.tsx

--- RootLayout.tsx
+++ RootLayout.tsx
@@ -1,1 +1,23 @@
+// import { Outlet } from "react-router";
+import { Outlet } from "@tanstack/react-router";
+import { NavMenu } from "@/components/NavMenu/nav-bar";
+import { ThemeProvider } from "@/contexts/theme-provider";
+import 'react-h5-audio-player/lib/styles.css';
 
+function Root() {
+  return (
+    <ThemeProvider defaultTheme="dark" storageKey="islamic-path-ui-theme">
+      <div className="w-full h-full">
+        <NavMenu />
+        <Outlet />
+        <footer className="p-6 text-center bg-background mt-8">
+          <div className="container">
+            <p>© 2024 IslamicPath. All rights reserved.</p>
+          </div>
+        </footer>
+      </div>
+    </ThemeProvider>
+  );
+}
+
+export default Root
